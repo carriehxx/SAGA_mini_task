829d4cd5feb06953f024d74e59239f3df42dc3d0
Huang Xin <142068979+carriehxx@users.noreply.github.com>
2024-07-28 12:56:06 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+const people = [
+  {
+    img: 'https://gracedgirl.com/wp-content/uploads/2022/08/how-to-be-a-personable-person-scaled.jpeg',
+    name: 'Sarah',
+    work: 'Project organizer',
+    text: `A dynamic and spirited project organizer with a knack for turning chaos into order. Her boundless energy and infectious enthusiasm inspire everyone around her. Sarah's razor-sharp wit and hilarious sense of humor make even the most challenging projects feel like a breeze. With her unique blend of organizational prowess and comedic flair, she not only keeps projects on track but also ensures that everyone has a great time along the way. Sarah is the heart and soul of any team, effortlessly blending professionalism with a fun-loving spirit. `,
+  },
+  {
+    img: 'https://res.cloudinary.com/dk0z4ums3/image/upload/v1639480293/attached_image/seluk-beluk-kepribadian-highly-sensitive-person.jpg',
+    name: 'Kate',
+    work: 'Teacher',
+    text: `A warm and caring teacher who has a heart of gold. With her gentle demeanor and nurturing approach, Kate creates a supportive and inclusive learning environment where every student feels valued. Her passion for teaching is evident in the way she patiently guides her students, always ready to offer a kind word of encouragement. Kate's genuine care for her students goes beyond the classroom, as she takes the time to understand their individual needs and aspirations. Her dedication and compassion make her a beloved figure in the community, inspiring both students and colleagues alike.`,
+  },
+  {
+    img: 'https://www.psychologs.com/wp-content/uploads/2024/01/8-tips-to-be-a-jolly-person.jpg',
+    name: 'Mike',
+    work: 'Developer',
+    text: `A highly professional developer with a heart for helping kids and people in need. His technical expertise is matched only by his compassion, as he channels his skills into creating impactful solutions for those who need them most. Mike's dedication to his craft is evident in every project he undertakes, always striving for excellence while keeping the end users in mind. His work is driven by a genuine desire to make a difference, and his efforts have brought positive changes to many lives. Whether he's developing educational tools for children or innovative resources for communities in need, Mike's professionalism and altruistic spirit shine through.`,
+  },
+  {
+    img: 'https://t4.ftcdn.net/jpg/03/83/25/83/360_F_383258331_D8imaEMl8Q3lf7EKU2Pi78Cn0R7KkW9o.jpg',
+    name: 'Joe',
+    work: 'Manager',
+    text: 'A manager who is the epitome of straightforwardness and enthusiasm. With her no-nonsense approach and clear communication style, Joe ensures that everyone is on the same page and knows exactly what needs to be done. Her enthusiasm is contagious, motivating her team to tackle challenges head-on with a positive attitude. Joe\'s directness and energy create an environment of efficiency and high morale, where goals are met with enthusiasm and a sense of purpose. She is a natural leader who inspires confidence and drives success, making her an invaluable asset to any organization.',
+  },
+];
+
+export default people;
---
+++ support.js
@@ -0,0 +1,71 @@
+import data from './data.js'
+const container = document.querySelector('.slide-container')
+const nextBtn = document.querySelector('.next-btn')
+const prevBtn = document.querySelector('.prev-btn')
+// if there is only one member 
+if (data.length === 1) {
+  nextBtn.style.display = 'none'
+  prevBtn.style.display = 'none'
+}
+// if more than 1 person
+let people = [...data]
+if (data.length === 2) {
+  people = [...data, ...data]
+}
+container.innerHTML = people
+  .map((person, slideIndex) => {
+    const { img, name, work, text } = person
+    let position = 'next'
+    if (slideIndex === 0) {
+      position = 'active'
+    }
+    if (slideIndex === people.length - 1) {
+      position = 'last'
+    }
+    if (data.length <= 1) {
+      position = 'active'
+    }
+    return `<article class="slide ${position}">
+  <img src=${img} class="img" alt="${name}"/>
+  <h4><strong>${name}</strong></h4>
+  <p class="title"><strong>${work}</strong></p>
+  <p class="text"><span>"</span>
+   ${text}<span>"</span>
+  </p>
+ </article>`
+  })
+  .join('')
+
+const startSlider = (type) => {
+  // get all three slides active,last, next
+  const active = document.querySelector('.active')
+  const last = document.querySelector('.last')
+  let next = active.nextElementSibling
+  if (!next) {
+    next = container.firstElementChild
+  }
+  active.classList.remove('active')
+  last.classList.remove('last')
+  next.classList.remove('next')
+
+  if (type === 'prev') {
+    active.classList.add('next')
+    last.classList.add('active')
+    next = last.previousElementSibling
+    if (!next) {
+      next = container.lastElementChild
+    }
+    next.classList.remove('next')
+    next.classList.add('last')
+    return
+  }
+  active.classList.add('last')
+  last.classList.add('next')
+  next.classList.add('active')
+}
+nextBtn.addEventListener('click', () => {
+  startSlider()
+})
+prevBtn.addEventListener('click', () => {
+  startSlider('prev')
+})
